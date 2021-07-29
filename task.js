ffbf07716f5b8e424bc6c826d6a7327dc182176d
raagul37 <87649564+raagul37@users.noreply.github.com>
2021-07-29 20:48:13 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,9 @@
+var request=new XMLHttpRequest();
+request.open('GET','https://api.covid19api.com/summary',true);
+request.send();
+request.onload=function(){
+    var data=JSON.parse(this.response);
+    console.log(data);
+    console.log(data.Global);
+
+}
