3175fc4fe9b8e2381ddcb242af45b0e1ef9cdfc3
raagul37 <87649564+raagul37@users.noreply.github.com>
2021-07-29 20:47:26 +0530

    Delete task.js

--- task.js
+++
@@ -1,8 +0,0 @@
-var request=new XMLHttpRequest();
-request.open('GET','https://api.covid19api.com/summary',true);
-request.send();
-request.onload=function(){
-    var data=JSON.parse(this.response);
-    console.log(data);
-
-}
